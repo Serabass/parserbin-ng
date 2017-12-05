4316caaf5f26fb57771dae402c1c6bf5d6d3641c
serab <is_everything@mail.ru>
2017-12-05 20:09:56 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# LarAng5 v1.0.2
+
+# Installation
+        $ git clone https://github.com/Serabass/larang5 --branch=v1.0.2 <your-app-name>
+        $ cd <your-app-name>
+        $ composer update
+
+Join and help me to make following projects:
+* ExpAng5 - Angular5 with Express
+* GrAng5 - Angular5 with Grails
+* AspAng5 - Angular5 with ASP.Net Core
+* PythAng5 - Angular5 with Python
+* RoRAng5 - Angular5 with Ruby On Rails
+* ElectrAng5 - Angular5 with Electron
---
+++ app/Console/Commands/Serve2.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Artisan;
+use Illuminate\Console\Command;
+
+class Serve2 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'serve2';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed|void
+     */
+    public function handle()
+    {
+        system('ng build');
+        Artisan::call('serve');
+    }
+}
